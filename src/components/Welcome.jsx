ad61987fae1017f7e8c1405feabaee9e88e1eeaf
Samridh Palleda <psamridh007@gmail.com>
2025-11-28 21:22:25 +0530

    Implemented welcome section

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+const Welcome = () => {
+  return (
+    <section id="welcome">
+      <p className="text-3xl font-[Georama] font-extralight">Hey, I'm Samridh! Welcome to my</p>
+      <h1 className="text-9xl font-[Georama] mt-7 italic">portfolio.</h1>
+    </section>
+  );
+};
+
+export default Welcome;
